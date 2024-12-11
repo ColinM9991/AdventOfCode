8515def2b4af8bab2e2e254b387a23b71eb237b6
Colin M <57466042+ColinM9991@users.noreply.github.com>
2024-12-11 19:35:53 +0000

    Day 3 Part 2

--- aoc_2024/src/day3.rs
+++ aoc_2024/src/day3.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
-fn solution_1(input: &str) -> u32 {
+fn multiply(input: &str) -> u32 {
     let re = Regex::new("mul\\((\\d+),(\\d+)\\)").unwrap();
 
     re.captures_iter(input)
@@ -12,6 +12,18 @@ fn solution_1(input: &str) -> u32 {
             )
         })
         .fold(0, |acc, (first, second)| acc + (first * second))
+}
+
+fn solution_1(input: &str) -> u32 {
+    multiply(input)
+}
+
+fn solution_2(input: &str) -> u32 {
+    let replace_re = Regex::new("don't\\(\\)[\\s\\S]*?do\\(\\)").unwrap();
+
+    let input = replace_re.replace_all(input, "");
+
+    multiply(&input)
 }
 
 #[cfg(test)]
@@ -29,6 +41,20 @@ mod tests {
     fn solution_1_real() {
         let input = include_str!("input_files/day3.txt");
 
-        assert_eq!(0, solution_1(input));
+        assert_eq!(188192787, solution_1(input));
+    }
+
+    #[test]
+    fn solution_2_example() {
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+
+        assert_eq!(48, solution_2(input));
+    }
+
+    #[test]
+    fn solution_2_real() {
+        let input = include_str!("input_files/day3.txt");
+
+        assert_eq!(113965544, solution_2(input));
     }
 }
